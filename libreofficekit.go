ca4715ab90bb5dc9c7618ae81fbd2c5de7bfe518
William Hutchinson <willhutchinson2.0@gmail.com>
2023-03-24 08:44:41 -0400

    feat: Include headers

--- libreofficekit.go
+++ libreofficekit.go
@@ -8,30 +8,9 @@ package libreofficekit
 import "C"
 import (
 	"fmt"
-	"image"
-	"strconv"
-	"strings"
 	"sync"
 	"unsafe"
 )
-
-// TwipsToPixels converts given twips to pixels with given dpi
-func TwipsToPixels(twips int, dpi int) int {
-	return int(float32(twips) / 1440.0 * float32(dpi))
-}
-
-// PixelsToTwips is like TwipsToPixels, but to another way
-func PixelsToTwips(pixels int, dpi int) int {
-	return int((float32(pixels) / float32(dpi)) * 1440.0)
-}
-
-// BGRA converts BGRA array of pixels to RGBA
-// https://github.com/golang/exp/blob/master/shiny/driver/internal/swizzle/swizzle_common.go#L13
-func BGRA(p []uint8) {
-	for i := 0; i < len(p); i += 4 {
-		p[i+0], p[i+2] = p[i+2], p[i+0]
-	}
-}
 
 type Office struct {
 	handle *C.struct__LibreOfficeKit
@@ -48,7 +27,7 @@ func NewOffice(path string) (*Office, error) {
 
 	lokit := C.lok_init(cPath)
 	if lokit == nil {
-		return nil, fmt.Errorf("Failed to initialize LibreOfficeKit with path: '%s'", path)
+		return nil, fmt.Errorf("failed to initialize LibreOfficeKit with path: '%s'", path)
 	}
 
 	office.handle = lokit
@@ -77,37 +56,11 @@ func (office *Office) LoadDocument(path string) (*Document, error) {
 	defer C.free(unsafe.Pointer(cPath))
 	handle := C.document_load(office.handle, cPath)
 	if handle == nil {
-		return nil, fmt.Errorf("Failed to load document")
+		return nil, fmt.Errorf("failed to load document")
 	}
 	document.handle = handle
 	return document, nil
 }
-
-func (office *Office) GetFilters() string {
-	filters := C.get_filter_types(office.handle)
-	defer C.free(unsafe.Pointer(filters))
-	return C.GoString(filters)
-}
-
-// Types of documents returned by Document.GetType function
-const (
-	TextDocument = iota
-	SpreadsheetDocument
-	PresentationDocument
-	DrawingDocument
-	OtherDocument
-)
-
-// Types of tile color mode
-const (
-	RGBATilemode = iota
-	BGRATilemode
-)
-
-const (
-	SetGraphicSelectionStart = iota
-	SetGraphicSelectionEnd
-)
 
 type Document struct {
 	handle *C.struct__LibreOfficeKitDocument
@@ -116,51 +69,6 @@ type Document struct {
 // Close destroys document
 func (document *Document) Close() {
 	C.destroy_document(document.handle)
-}
-
-// GetType returns type of loaded document
-func (document *Document) GetType() int {
-	return int(C.get_document_type(document.handle))
-}
-
-// GetParts returns count of slides (for presentations) or pages (for text documents)
-func (document *Document) GetParts() int {
-	return int(C.get_document_parts(document.handle))
-}
-
-// GetPart returns current part of document, e.g.
-// if document was just loaded it's current part will be 0
-func (document *Document) GetPart() int {
-	return int(C.get_document_part(document.handle))
-}
-
-// SetPart updates current part of document
-func (document *Document) SetPart(part int) {
-	C.set_document_part(document.handle, C.int(part))
-}
-
-// GetPartName returns current slide title (for presentations) or page title (for text documents)
-func (document *Document) GetPartName(part int) string {
-	cPart := C.int(part)
-	cPartName := C.get_document_part_name(document.handle, cPart)
-	defer C.free(unsafe.Pointer(cPartName))
-	return C.GoString(cPartName)
-}
-
-// GetSize returns width and height of document in twips (1 Twip = 1/1440th of an inch)
-// You can convert twips to pixels by this formula: (width or height) * (1.0 / 1440.0) * DPI
-func (document *Document) GetSize() (int, int) {
-	width := C.long(0)
-	heigth := C.long(0)
-	C.get_document_size(document.handle, &width, &heigth)
-	return int(width), int(heigth)
-}
-
-// InitializeForRendering must be called before performing any rendering-related actions
-func (document *Document) InitializeForRendering(arguments string) {
-	cArguments := C.CString(arguments)
-	defer C.free(unsafe.Pointer(cArguments))
-	C.initialize_for_rendering(document.handle, cArguments)
 }
 
 // SaveAs saves document at desired path in desired format with applied filter rules
@@ -174,83 +82,7 @@ func (document *Document) SaveAs(path string, format string, filter string) erro
 	defer C.free(unsafe.Pointer(cFilter))
 	status := C.document_save(document.handle, cPath, cFormat, cFilter)
 	if status != 1 {
-		return fmt.Errorf("Failed to save document")
+		return fmt.Errorf("failed to save document")
 	}
 	return nil
-}
-
-// CreateView return id if newly created view
-func (document *Document) CreateView() int {
-	return int(C.create_view(document.handle))
-}
-
-// GetView returns current document view id
-func (document *Document) GetView() int {
-	return int(C.get_view(document.handle))
-}
-
-// GetViews returns total number of views in document
-func (document *Document) GetViewsCount() int {
-	return int(C.get_views_count(document.handle))
-}
-
-// GetTileMode returns tile mode of document, currently only RGBA or BGRA (5.2).
-// You can compare returned int with RGBATilemode / BGRATilemode.
-func (document *Document) GetTileMode() int {
-	return int(C.get_tile_mode(document.handle))
-}
-
-func (document *Document) GetTextSelection(mimetype string) string {
-	cMimetype := C.CString(mimetype)
-	defer C.free(unsafe.Pointer(cMimetype))
-	return C.GoString(C.get_text_selection(document.handle, cMimetype))
-}
-
-func (document *Document) SetTextSelection(sType int, x int, y int) {
-	C.set_text_selection(
-		document.handle,
-		C.int(sType),
-		C.int(x),
-		C.int(y),
-	)
-}
-
-func (document *Document) ResetTextSelection() {
-	C.reset_selection(document.handle)
-}
-
-// GetPartPageRectangles array of image.Rectangle, with actually TextDocument page
-// rectangles. Useful, when rendering text document page-by-page.
-func (document *Document) GetPartPageRectangles() []image.Rectangle {
-	var rectangles []image.Rectangle
-	rawRectangles := C.GoString(C.get_part_page_rectangles(document.handle))
-	pageRectangles := strings.Split(rawRectangles, ";")
-	for _, points := range pageRectangles {
-		var intPoints []int
-		strPoints := strings.Split(points, ",")
-		for _, point := range strPoints {
-			point = strings.Trim(point, " ")
-			i, _ := strconv.Atoi(point)
-			intPoints = append(intPoints, i)
-		}
-		x0, y0 := intPoints[0], intPoints[1]
-		x1, y1 := x0+intPoints[2], y0+intPoints[3]
-		rectangles = append(rectangles, image.Rect(x0, y0, x1, y1))
-	}
-	return rectangles
-}
-
-// PaintTile renders tile to given buf (which size must be a `4 * canvasWidth * canvasHeight`).
-// In practice buf must be a pointer to image.Image.Pix array's first element, e.g. unsafe.Pointer(&image.Pix[0])
-func (document *Document) PaintTile(buf unsafe.Pointer, canvasWidth int, canvasHeight int, tilePosX int, tilePosY int, tileWidth int, tileHeight int) {
-	C.paint_tile(
-		document.handle,
-		(*C.uchar)(buf),
-		(C.int)(canvasWidth),
-		(C.int)(canvasHeight),
-		(C.int)(tilePosX),
-		(C.int)(tilePosY),
-		(C.int)(tileWidth),
-		(C.int)(tileHeight),
-	)
 }
--- lokbridge.h
+++ lokbridge.h
@@ -1,6 +1,6 @@
 #include <stdlib.h>
-#include "LibreOfficeKit/LibreOfficeKitInit.h"
-#include "LibreOfficeKit/LibreOfficeKit.h"
+#include "./LibreOfficeKitInit.h"
+#include "./LibreOfficeKit.h"
 
 
 void destroy_office(LibreOfficeKit* pThis) {
@@ -15,10 +15,6 @@ void free_error(LibreOfficeKit* pThis, char* message) {
     return pThis->pClass->freeError(message);
 }
 
-char* get_filter_types(LibreOfficeKit* pThis) {
-    return pThis->pClass->getFilterTypes(pThis);
-};
-
 LibreOfficeKitDocument* document_load(LibreOfficeKit* pThis, const char* pURL) {
     return pThis->pClass->documentLoad(pThis, pURL);
 };
@@ -27,69 +23,6 @@ void destroy_document(LibreOfficeKitDocument* pThis) {
     return pThis->pClass->destroy(pThis);
 };
 
-void get_document_size(LibreOfficeKitDocument* pThis, long* pWidth, long* pHeight) {
-    return pThis->pClass->getDocumentSize(pThis, pWidth, pHeight);
-};
-void set_document_part(LibreOfficeKitDocument* pThis, int nPart) {
-    return pThis->pClass->setPart(pThis, nPart);
-};
-
-int get_document_type(LibreOfficeKitDocument* pThis) {
-    return pThis->pClass->getDocumentType(pThis);
-};
-
-int get_document_parts(LibreOfficeKitDocument* pThis) {
-    return pThis->pClass->getParts(pThis);
-};
-
-int get_document_part(LibreOfficeKitDocument* pThis) {
-    return pThis->pClass->getPart(pThis);
-};
-
-char* get_document_part_name(LibreOfficeKitDocument* pThis, int nPart) {
-    return pThis->pClass->getPartName(pThis, nPart);
-};
-
-void initialize_for_rendering(LibreOfficeKitDocument* pThis, const char* pArguments) {
-    return pThis->pClass->initializeForRendering(pThis, pArguments);
-};
-
 int document_save(LibreOfficeKitDocument* pThis, const char* pUrl, const char* pFormat, const char* pFilterOptions) {
     return pThis->pClass->saveAs(pThis, pUrl, pFormat, pFilterOptions);
-};
-
-int create_view(LibreOfficeKitDocument* pThis) {
-    return pThis->pClass->createView(pThis);
-};
-
-int get_view(LibreOfficeKitDocument* pThis) {
-    return pThis->pClass->getView(pThis);
-};
-
-int get_views_count(LibreOfficeKitDocument* pThis) {
-    return pThis->pClass->getViewsCount(pThis);
-};
-
-void paint_tile(LibreOfficeKitDocument* pThis, unsigned char* pBuffer, const int nCanvasWidth, const int nCanvasHeight, const int nTilePosX, const int nTilePosY, const int nTileWidth, const int nTileHeight) {
-    return pThis->pClass->paintTile(pThis, pBuffer, nCanvasWidth, nCanvasHeight, nTilePosX, nTilePosY, nTileWidth, nTileHeight);
-};
-
-int get_tile_mode(LibreOfficeKitDocument* pThis) {
-    return pThis->pClass->getTileMode(pThis);
-};
-
-char* get_part_page_rectangles(LibreOfficeKitDocument* pThis) {
-    return pThis->pClass->getPartPageRectangles(pThis);
-};
-
-void set_text_selection(LibreOfficeKitDocument* pThis, int nType, int nX, int nY) {
-    return pThis->pClass->setTextSelection(pThis, nType, nX, nY);
-};
-
-void reset_selection(LibreOfficeKitDocument* pThis) {
-    return pThis->pClass->resetSelection(pThis);
-};
-
-char* get_text_selection(LibreOfficeKitDocument* pThis, const char* pMimeType) {
-    return pThis->pClass->getTextSelection(pThis, pMimeType, NULL);
 };
